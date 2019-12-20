619a766bdf3ee3ac3044a40be1d438ed55c70193
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-12-20 10:05:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function rectClick(e) {
             results[key] = rooms.levels["3"][key]
         }
     }
-    console.log(results[id]);
     console.log(results);
     str = results[id].ladder;
     document.querySelector('#leftBlock').innerHTML += `${str}<br>`;
@@ -17,11 +16,8 @@ function rectClick(e) {
 document.addEventListener('DOMContentLoaded', function() {
     classes = document.getElementsByTagName('svg').item(0).getElementById('classes');
     rects = classes.getElementsByTagName('g');
-    for (let i = 0; i < rects.length; i++) {
-        const el = rects[i];
+    for (el of rects) {
         console.log(el);
-        el.addEventListener('click', function(e) {
-            rectClick(e)
-        })
+        el.addEventListener('click', e => rectClick(e));
     }
 })
