0be3b62ffd63ebe4c4cef822cf6a73f5aab6f7a4
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-12-20 09:56:38 +0300

    auto adding onclick

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function rectClick(e) {
     str = results[id].ladder;
     document.querySelector('#leftBlock').innerHTML += `${str}<br>`;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    classes = document.getElementsByTagName('svg').item(0).getElementById('classes');
+    rects = classes.getElementsByTagName('g');
+    for (let i = 0; i < rects.length; i++) {
+        const el = rects[i];
+        console.log(el);
+        el.addEventListener('click', function(e) {
+            rectClick(e)
+        })
+    }
+})
