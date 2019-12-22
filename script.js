d49b5b6a2423b65b0531adfa7fc08f20bf90e2ce
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-12-22 14:26:50 +0300

    style, hover, colors, closable display

--- script.js
+++ script.js
@@ -10,26 +10,42 @@ function rectClick(e) {
     }
     console.log(results);
     str = results[id];
+    rdbleName = e.target.parentNode.getElementsByTagName('text')[0].getElementsByTagName('tspan')[0].innerHTML;
+    document.querySelector('#display').classList.add('hover');
+    document.querySelector('#display').style.display = 'block';
+
     document.querySelector('#display').innerHTML = '';
     if (id)
-        document.querySelector('#display').innerHTML += `<h1>${id}</h1><br>`;
+        document.querySelector('#display').innerHTML += `<h1>${rdbleName}</h1><br>`;
     if (str.ladder)
         document.querySelector('#display').innerHTML += `<p>located near the <b>${str.ladder}</b> ladder</p><br>`;
     if (str.name)
         document.querySelector('#display').innerHTML += `<p>previously named <b>${str.name}</b></p><br>`;
 }
+function closeDisplay() {
+    document.querySelector('#display').style.display = 'none';
+}
+document.onkeyup = e => {
+    if (e.code === 'Escape')
+        closeDisplay()
+};
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     classes = document.getElementsByTagName('svg').item(0).getElementById('classes');
     rects = classes.getElementsByTagName('g');
     for (el of rects) {
         console.log(el);
+        
         el.onclick = e => rectClick(e);
         el.addEventListener('mouseenter', e => {
-            e.target.getElementsByTagName('rect')[0].setAttribute('opacity', 0.2)
+            e.target.getElementsByTagName('rect')[0].setAttribute('opacity', 0.2);
+            if (document.getElementById('display').getElementsByTagName('h1')[0].innerHTML === e.target.getElementsByTagName('text')[0].getElementsByTagName('tspan')[0].innerHTML)
+                document.getElementById('display').classList.add('hover');
+
         })
         el.addEventListener('mouseleave', e => {
-            e.target.getElementsByTagName('rect')[0].setAttribute('opacity', 0.1)
+            e.target.getElementsByTagName('rect')[0].setAttribute('opacity', 0.1);
+            document.getElementById('display').classList.remove('hover');
         })
     }
 })
