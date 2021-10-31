6e109badcf94a26cc6e097f1eef9b35990d55945
artginzburg <art.ginzburg@ya.ru>
2021-10-31 20:35:30 +0300

    make index.js code more readable

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,36 +7,50 @@ import {
 } from '../utils/constants.js';
 import { roomDisplay } from '../utils/elements.js';
 
-const digitString = 'Digit';
-const arrowString = 'Arrow';
-const escapeString = 'Escape';
+const keyCodes = {
+  digit: 'Digit',
+  arrow: 'Arrow',
+  escape: 'Escape',
+};
 
 const structurePath = './src/utils/structure.json';
 
 document.addEventListener('DOMContentLoaded', schoolMap.loadAll);
 
 document.body.onkeydown = (e) => {
-  if (e.code === escapeString)
-    return roomDisplay.classList.contains(displayOpenedClass)
+  if (e.code.includes(keyCodes.escape)) {
+    roomDisplay.classList.contains(displayOpenedClass)
       ? schoolMap.display.close()
       : schoolMap.floor.deSelect();
+    return;
+  }
 
-  if (e.code.includes(digitString)) return schoolMap.floor.select('', e.code.split(digitString)[1]);
+  if (e.code.includes(keyCodes.digit)) {
+    schoolMap.floor.select('', e.code.split(keyCodes.digit)[1]);
+    return;
+  }
 
-  if (e.code.includes(arrowString)) {
+  if (e.code.includes(keyCodes.arrow)) {
     const allSelected = document.querySelector(selectedSelector);
-    let currentId = allSelected ? allSelected.id : 0;
+    const currentId = allSelected ? Number(allSelected.id) : 0;
 
-    const way = e.code.split(arrowString)[1];
+    const way = e.code.split(keyCodes.arrow)[1];
 
     if (way === 'Down' || way === 'Right') {
-      if (!currentId) currentId = floorsQuantity + 1;
-      schoolMap.floor.select('', currentId - 1);
-    } else if (way === 'Up' || way === 'Left') schoolMap.floor.select('', Number(currentId) + 1);
+      schoolMap.floor.select('', (currentId || floorsQuantity + 1) - 1);
+      return;
+    }
+    if (way === 'Up' || way === 'Left') {
+      schoolMap.floor.select('', currentId + 1);
+    }
   }
 };
 
-document.body.onclick = (e) => e.target.closest(floorSelector) ?? schoolMap.floor.deSelect();
+document.body.onclick = (e) => {
+  if (!e.target.closest(floorSelector)) {
+    schoolMap.floor.deSelect();
+  }
+};
 
 fetch(structurePath)
   .then((res) => res.json())
